adde916a77afc285053ce08a3c7a62b15881a139
Yastrebvitali <yastrebvitali@gmail.com>
2022-02-21 15:31:11 +0300

    save

--- index.js
+++ index.js
@@ -3,28 +3,32 @@
 
 
 
-function Task(name){
+function NewWorks(name){
     this.id         = 'id';
     this.name    = name;
     this.isSelected = false;
 }
 
 function TodoList () {
-    this.tasks = [];
 
+    this.NewWorks = [];
+
+    /*----------------------------------------------------*/
+    this.createTask = function(NewWorks){
+        document.querySelector('.newWorks').value
+        var result= new newWorks(document.querySelector('.newWorks').value);
+        return   result;
+    };
+    /*----------------------------------------------------*/
     this.init= function (){
-        var fromStorage = localStorage.getItem('Task');
+        var fromStorage = localStorage.getItem('NewWorks');
         if (fromStorage) {
             document.querySelector('.listNewWorks').innerHTML = fromStorage;
         }
         document.querySelector('.btnAdd').addEventListener('change', this.createTask);
         //document.addEventListener('click', this.action.bind(this));
     };
-    this.createTask = function(task){
-
-        this.name=document.querySelector('.newWorks').value;
-
-    };
+    /*----------------------------------------------------*/
     this.updateTask = function(){
 
     };
@@ -34,12 +38,29 @@ function TodoList () {
 
 };
 
+var btnCases= querySelector('.btnAdd');
+
+btnCases.addEventListener('click', () => {
+    tasks.push(new Task(enter_cases_input.value));
+
+});
+
+
+
+
+enter_cases_input.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
+        tasks.push(new Task(enter_cases_input.value));
+
+    }
+});
+
 var result = new TodoList();
 
 //console.log(new Task('luk true ' , false));
 //console.log(new Task( 'morkov', true));
 
- //console.log('result', result.init());
+ console.log('result', result.createTask());
 
 
 
